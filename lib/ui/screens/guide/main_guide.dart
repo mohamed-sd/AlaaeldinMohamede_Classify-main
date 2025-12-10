710a60691dba56c9668b28693871911ff0f491c2
ahsam <ah.samba12@gmail.com>
2025-12-10 09:46:05 +0200

    update splah screen & Desginmain_guide_card.dart to new Desgin from mohiad

--- lib/ui/screens/guide/main_guide.dart
+++ lib/ui/screens/guide/main_guide.dart
@@ -13,12 +13,14 @@ class MainGuide extends StatelessWidget {
       appBar: AppBar(
         backgroundColor: context.color.mainBrown,
         title: Text("وزارة المعادن"),
+
+
       ),
       body: Container(
         padding: EdgeInsets.all(16),
         child: GridView.count(
           crossAxisCount: 2,
-          crossAxisSpacing: 20,
+          crossAxisSpacing: 10,
           children: [
             // The main guide wedgit :
             GestureDetector(
@@ -27,10 +29,14 @@ class MainGuide extends StatelessWidget {
               },
               child: MainGuideCard(
                 title: "تراخيص التعدين",
+                urlimg: "https://firebasestorage.googleapis.com/v0/b/miningmarket-firebase.appspot.com/o/Bareeq%2F%D9%88%D8%B2%D8%A7%D8%B1%D8%A9%20%D8%A7%D9%84%D9%85%D8%B9%D8%A7%D8%AF%D9%86%20(1).jpg?alt=media&token=f66650db-36a5-4924-b56f-ac526402990e",
+
               ),
             ),
             MainGuideCard(
-              title: "عقود التعدين واستخراج الصخور والمعادن الصناعية",
+              title: "عقود التعدين",
+              urlimg: "https://firebasestorage.googleapis.com/v0/b/miningmarket-firebase.appspot.com/o/Bareeq%2F%D9%88%D8%B2%D8%A7%D8%B1%D8%A9%20%D8%A7%D9%84%D9%85%D8%B9%D8%A7%D8%AF%D9%86%20(2).jpg?alt=media&token=f86a02f4-ab02-42b8-944d-05137a797523",
+
             ),
           ],
         ),
--- lib/ui/screens/widgets/equipation_widgets/main_guide_card.dart
+++ lib/ui/screens/widgets/equipation_widgets/main_guide_card.dart
@@ -5,38 +5,98 @@ import 'package:flutter/material.dart';
 
 class MainGuideCard extends StatelessWidget {
   String title;
-  MainGuideCard({super.key, required this.title});
+  String urlimg;
+
+  MainGuideCard({super.key, required this.title , required this.urlimg});
+
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-          color: context.color.mainBrown,
-          borderRadius: BorderRadius.only(
-            topLeft: Radius.circular(18),
-            bottomRight: Radius.circular(18),
-          )),
-      child: Container(
-        decoration: BoxDecoration(
-            color: context.color.mainGold,
-            borderRadius: BorderRadius.only(
-              topLeft: Radius.circular(100),
-              bottomRight: Radius.circular(100),
-            )),
-        child: Center(
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 25),
-            child: Text(
-              title,
-              style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 15,
-                  fontWeight: FontWeight.bold),
-              textAlign: TextAlign.center,
-            ),
-          ),
-        ),
-      ),
-    );
+    return  Expanded(
+        child: Padding(
+            padding: EdgeInsets.all(3),
+            child: Material(
+                color: Colors.transparent,
+                elevation: 5,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(15),
+                ),
+                child: Container(
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius:
+                      BorderRadius.circular(15),
+                    ),
+                    child: Padding(
+                        padding: EdgeInsets.all(5),
+                        child: Column(
+                            mainAxisSize:
+                            MainAxisSize.max,
+                            mainAxisAlignment:
+                            MainAxisAlignment.center,
+                            children: [
+                              ClipRRect(
+                                borderRadius:
+                                BorderRadius.circular(
+                                    10),
+                                child: Image.network(
+                                  urlimg,
+                                  width: double.infinity,
+                                  height:
+                                  MediaQuery.sizeOf(
+                                      context)
+                                      .height *
+                                      0.15,
+                                  fit: BoxFit.fill,
+                                ),
+                              ),
+                              Padding(
+                                  padding:
+                                  EdgeInsetsDirectional
+                                      .fromSTEB(
+                                      0, 5, 0, 5),
+                                  child: Row(
+                                      mainAxisSize:
+                                      MainAxisSize
+                                          .max,
+                                      children: [
+                                        Flexible(
+                                            child: Container(
+                                                width: double.infinity,
+                                                decoration: BoxDecoration(
+                                                  color: Color(0xff844F03),
+                                                  borderRadius:
+                                                  BorderRadius.circular(5),
+                                                ),
+                                                child: Padding(
+                                                    padding: EdgeInsetsDirectional.fromSTEB(5, 0, 5, 0),
+                                                    child: Row(mainAxisSize: MainAxisSize.max, children: [
+                                                      Expanded(
+                                                        child: Align(
+                                                            alignment: AlignmentDirectional(0, 0),
+                                                            child: Padding(
+                                                              padding: EdgeInsetsDirectional.fromSTEB(0, 3, 0, 3),
+                                                              child: Text(title,
+                                                                  style: TextStyle(
+                                                                    color: Colors.white,
+                                                                    fontSize: 12,
+                                                                    letterSpacing: 0.0,
+                                                                    fontWeight: FontWeight.w700,
+                                                                  )),
+                                                            )),
+                                                      )
+                                                    ])))),
+                                      ])),
+
+
+
+
+
+                            ]
+                        )
+                    )
+                )
+            )));
   }
 }
